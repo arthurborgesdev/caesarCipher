2962e5d3ec1cf65c36499649aae6f5609c94bf61
Arthur Borges <38326017+arthurborgesdev@users.noreply.github.com>
2020-09-17 14:45:56 -0300

    Another way of doing things (from the future)

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,6 +8,14 @@ def caesar_cipher(str, num)
   }.join('')
 end
 
+# Refactoring of 2nd time doing Open App Academy 17-09-2020
+def caesar_cipher(str, num)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  converted = ""
+  str.each_char { |char| converted += alphabet[(alphabet.index(char) + num) % 26] }
+  converted
+end
+
 puts caesar_cipher("apple", 1)    #=> "bqqmf"
 puts caesar_cipher("bootcamp", 2) #=> "dqqvecor"
 puts caesar_cipher("zebra", 4)    #=> "difve"
